69c4419921064d0da96731ced27f837a93f8b5ee
Ali-Eldeba <alieldeba375@gmail.com>
2022-07-30 21:53:18 +0200

    fixing bugs

--- src/slices/calculateSlice.js
+++ src/slices/calculateSlice.js
@@ -9,8 +9,7 @@ const calculateSlice = createSlice({
   name: "calculate",
   initialState,
   reducers: {
-    // here i increment the state by 0.5 because ui renders 2 times, so it will be 1
-    addPoint: (state) => void (state.score += 0.5),
+    addPoint: (state) => void (state.score += 1),
     toggleCalculate: (state) => void (state.calculate = !state.calculate),
   },
 });
