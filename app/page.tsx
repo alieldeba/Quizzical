8eddb04279d11bf6006a84f5cbb6713ab80b10d8
alieldeba <alieldeba375@gmail.com>
2023-09-14 12:14:40 +0300

    converting start page theme with next and shadcn

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import Start from "@/components/Start";
+
+export default function App() {
+  return (
+    <>
+      <ModeToggle />
+      <Start />
+    </>
+  );
+}
